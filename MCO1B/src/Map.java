c5391ad491303bb2c5e7b0a1fed24a05618f45a9
rijopalla <rij_opalla@dlsu.edu.ph>
2021-04-13 22:48:45 +0800

    added getters for character superiority, base amt, elementcombomultiplie

--- MCO1B/src/Map.java
+++ MCO1B/src/Map.java
@@ -5,13 +5,6 @@ public class Map {
 	private int mapBaseAmount;
 	private Enemy[] mapEnemyList;
 	
-	//Adventure variables
-	private float elementComboMultiplier;
-	private float totalFinalWeaponPower;
-	private float totalCharacterInfluence;
-	private int   totalResource;
-	private float characterSuperiority;
-	private int   enemySuperiority;
 	
 	//Constructor
 	public Map(String name, int amount) {
@@ -20,6 +13,12 @@ public class Map {
 	}
 	
 	//Methods
+	
+	//Getters
+	public int getBaseAmount() {
+		return this.mapBaseAmount;
+	}
+	
 	private float calculateTotalFinalWeaponPower(Weapon char1Weapon, Weapon char2Weapon) {
 		
 		float finalWeaponPower1;
@@ -63,8 +62,174 @@ public class Map {
 		return totalCharInfluence;
 	}
 	
-	public void adventure(Map map, Character char1, Character char2) {
-		//TBA hehe
+	private float getElementComboMultiplier(Character char1, Character char2) {
+		
+		String charElement1 = char1.getCharacterElement().toLowerCase(); //turns char1's element to lowercase characters
+		String charElement2 = char2.getCharacterElement().toLowerCase(); //turns char2's element to lowercase characters
+		
+		float multiplier = 0;
+		float normalPair = (float) 0.1;
+		float perfectPair = (float) 0.75;
+		float nicePair = (float) 0.5;
+		float decentPair = (float) 0.25;
+		float noEffect = 0;
+		float badPair = (float) -0.25;
+		
+		
+		if (charElement1.equalsIgnoreCase(charElement2)) //if elements are the same
+			multiplier = normalPair;
+		else if (charElement1.equalsIgnoreCase("joker")) { //if the first element is joker:
+			
+			switch (charElement2) {
+			case "trigger":
+				multiplier = decentPair;
+				break;
+			case "metal":
+				multiplier = decentPair;
+				break;
+			case "cyclone":
+				multiplier = perfectPair;
+				break;
+			case "luna":
+				multiplier = nicePair;
+				break;
+			case "heat":
+				multiplier = nicePair;
+				break;
+			}
+		}
+		else if (charElement1.equalsIgnoreCase("trigger")) { //if the first element is trigger:
+			
+			switch (charElement2) {
+			case "joker":
+				multiplier = decentPair;
+				break;
+			case "metal":
+				multiplier = noEffect;
+				break;
+			case "cyclone":
+				multiplier = noEffect;
+				break;
+			case "luna":
+				multiplier = perfectPair;
+				break;
+			case "heat":
+				multiplier = noEffect;
+				break;
+			}
+		}
+		else if (charElement1.equalsIgnoreCase("metal")) { //if the first element is metal:
+			
+			switch (charElement2) {
+			case "joker":
+				multiplier = decentPair;
+				break;
+			case "trigger":
+				multiplier = noEffect;
+				break;
+			case "cyclone":
+				multiplier = decentPair;
+				break;
+			case "luna":
+				multiplier = decentPair;
+				break;
+			case "heat":
+				multiplier = perfectPair;
+				break;
+			}
+		}
+		else if (charElement1.equalsIgnoreCase("cyclone")) { //if the first element is cyclone:
+			
+			switch (charElement2) {
+			case "joker":
+				multiplier = decentPair;
+				break;
+			case "trigger":
+				multiplier = decentPair;
+				break;
+			case "metal":
+				multiplier = perfectPair;
+				break;
+			case "luna":
+				multiplier = nicePair;
+				break;
+			case "heat":
+				multiplier = nicePair;
+				break;
+			}
+		}
+		else if (charElement1.equalsIgnoreCase("luna")) { //if the first element is luna:
+			
+			switch (charElement2) {
+			case "joker":
+				multiplier = nicePair;
+				break;
+			case "trigger":
+				multiplier = perfectPair;
+				break;
+			case "metal":
+				multiplier = decentPair;
+				break;
+			case "cyclone":
+				multiplier = badPair;
+				break;
+			case "heat":
+				multiplier = badPair;
+				break;
+			}
+		}
+		else if (charElement1.equalsIgnoreCase("heat")) { //if the first element is heat:
+			
+			switch (charElement2) {
+			case "joker":
+				multiplier = nicePair;
+				break;
+			case "trigger":
+				multiplier = noEffect;
+				break;
+			case "metal":
+				multiplier = perfectPair;
+				break;
+			case "cyclone":
+				multiplier = badPair;
+				break;
+			case "luna":
+				multiplier = badPair;
+				break;
+			}
+		}
+		
+		return multiplier;
+		
 	}
+	
+	private float getCharacterSuperiority(Character char1, Character char2) {
+										
+		//calculate total final weapon power
+		float totalFinalWeaponPower = this.calculateTotalFinalWeaponPower(char1.getCharacterWeapon(),
+									  char2.getCharacterWeapon()); //assumes char1 and 2 already have a weapon equipped
+		
+		//calculate total char influence
+		float totalCharacterInfluence = this.calculateTotalCharacterInfluence(char1, char2);
+		
+		//calculate character superiority
+		float characterSuperiority = totalFinalWeaponPower * (totalCharacterInfluence / 10);
+		
+		return characterSuperiority;
+	}
+	
+	public void adventure(Map map, Character char1, Character char2, Resource resourceNum) {
+		//TBA hehe
+		//Adventure variables
+		
+		float elementComboMultiplier;
+		float totalFinalWeaponPower;
+		float totalCharacterInfluence;
+		int   totalResource;
+		float characterSuperiority;
+		int   enemySuperiority;
+	}
+	
+
 
 }
