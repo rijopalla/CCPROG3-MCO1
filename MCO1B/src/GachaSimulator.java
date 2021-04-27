dd3bdad59881c44eb5357e673e5183a78fd3b9b5
rijopalla <rij_opalla@dlsu.edu.ph>
2021-04-27 23:55:39 +0800

    added main class (GachaSimulator); will add more in the future

---
+++ MCO1B/src/GachaSimulator.java
@@ -0,0 +1,37 @@
+// main/driver class
+import java.util.Scanner;
+
+public class GachaSimulator {
+
+	public static void main(String[] args) {
+		
+		Scanner stringInput = new Scanner(System.in); //takes strings as inputs
+		Scanner numInput    = new Scanner(System.in); //takes integers as inputs
+		
+		//Intro message
+		System.out.println("Welcome to the Gacha Simulator!");
+		System.out.printf("\n");
+		System.out.println("Please enter your username: ");
+		
+		String playerName = stringInput.nextLine(); //take user input as player name
+		stringInput.close();
+		
+		Player player = new Player(playerName); //create a new player
+		GachaMachine machine = new GachaMachine(); //create new gacha machine object
+		
+		//TODO: Perform 1 multiroll for characters, store it to player inventory
+		
+		//TODO: Perform 1 multiroll for weapons, store it to player inventory
+		
+		System.out.printf("Hello %s!\n", player.getPlayerName());
+		System.out.println("Before you start adventuring, you'll need characters and weapons!");
+		System.out.println("Fortunately for you, you already start with 10 of each!");
+		
+		
+		//TODO: Display player inventory (both character & weapons)
+		
+		//TODO: ask player what they want to do next (Manage characters/weapons [this includes merging characters/weap, level up, equip/unequip], or go on an adventure)
+		//TODO: implement main loop
+	}
+
+}
