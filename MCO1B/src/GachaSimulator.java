d4694e664298f52c0e1359eb8b56df824e53f123
rijopalla <rij_opalla@dlsu.edu.ph>
2021-04-29 21:42:13 +0800

    added adventure branch in main loop

--- MCO1B/src/GachaSimulator.java
+++ MCO1B/src/GachaSimulator.java
@@ -6,8 +6,7 @@ public class GachaSimulator {
 
 	public static void main(String[] args) {
 		
-		Scanner stringInput = new Scanner(System.in); //takes strings as inputs
-		Scanner numInput    = new Scanner(System.in); //takes integers as inputs
+		Scanner input = new Scanner(System.in); //scanner variable that takes user inputs
 		int userChoice; // int variable for user actions 
 		boolean isActive = true; //used in main loop
 		
@@ -24,8 +23,7 @@ public class GachaSimulator {
 		System.out.printf("\n");
 		System.out.println("Please enter your username: ");
 		
-		String playerName = stringInput.nextLine(); //take user input as player name
-		stringInput.close();
+		String playerName = input.nextLine(); //take user input as player name
 		
 		Player player = new Player(playerName); //create a new player
 		GachaMachine machine = new GachaMachine(); //create new gacha machine object
@@ -48,28 +46,74 @@ public class GachaSimulator {
 		System.out.printf("\n");
 		
 		//Display player inventory (both character & weapons)
-		player.displayInventory();
+		player.displayCharInventory();
+		player.displayWeaponInventory();
+		
 		System.out.printf("\n");
 		
 		//TODO: ask player what they want to do next (Manage characters/weapons [this includes merging characters/weap, level up, equip/unequip], or go on an adventure)
-		//TODO: implement main loop
-		while (isActive) {
+		while (isActive) { //main loop
 			
 			System.out.println("What action do you want do to next?(Enter 1 or 2)");
 			System.out.println("1. Go on an adventure");
 			System.out.println("2. Manage Characters/Weapons");
-			userChoice = numInput.nextInt();
-			numInput.close();
+			userChoice = input.nextInt();
 			
 			if (userChoice == 1) {
-				//TODO:adventures here
 				System.out.println("-----------Adventure-----------");
+				System.out.println("Select two characters: ");
+				
+				//int variables that store the index of chosen character (based on user input):
+				int charIndex1 = 0;
+				int charIndex2 = 0;
+				
+				for (int i = 0; i < 2; i++) {
+					
+					player.displayCharInventory();
+					
+					if (i == 0)
+						charIndex1 = input.nextInt();
+					if (i == 1)
+						charIndex2 = input.nextInt();
+					
+					if (charIndex1 == charIndex2)
+						System.out.println("Error! Choose another character!");
+				}
+				
+				System.out.println("Choose a map: ");
+				
+				for (int j = 0; j < mapList.size(); j++) { //displays map names
+					System.out.printf("[%d] %s\n", j, mapList.get(j).getMapName());
+				}
+				
+				int mapChoice = input.nextInt();
+				
+				switch(userChoice) {
+				case 0: //underground caverns
+					//player gets resources from the map and the total no. of resources is increased
+					player.addResource(mapList.get(mapChoice).adventure(player.getPlayerCharacter(charIndex1), player.getPlayerCharacter(charIndex2)));
+					break;
+				case 1: //forest of enchantments
+					player.addResource(mapList.get(mapChoice).adventure(player.getPlayerCharacter(charIndex1), player.getPlayerCharacter(charIndex2)));
+					break;
+				case 2: //sea of hope
+					player.addResource(mapList.get(mapChoice).adventure(player.getPlayerCharacter(charIndex1), player.getPlayerCharacter(charIndex2)));
+					break;
+				case 3: //tower of ether
+					player.addResource(mapList.get(mapChoice).adventure(player.getPlayerCharacter(charIndex1), player.getPlayerCharacter(charIndex2)));
+					break;
+				case 4: //celestial plane
+					player.addResource(mapList.get(mapChoice).adventure(player.getPlayerCharacter(charIndex1), player.getPlayerCharacter(charIndex2)));
+					break;
+				}
+				
 			}
 			else {
 				//TODO: More options for managing characters/weapon
+				System.out.println("---------Management-------------");
+				
 			}
 		}
 		
 	}
-
 }
