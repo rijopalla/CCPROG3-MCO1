a333445eb5daee3d7c5924ad3872e30583d9c34d
rijopalla <rij_opalla@dlsu.edu.ph>
2021-04-29 15:36:05 +0800

    modified loop in displayInventory()

--- MCO1B/src/Player.java
+++ MCO1B/src/Player.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Iterator;
-
 public class Player {
 
 	//Properties
@@ -53,42 +51,34 @@ public class Player {
 	public void displayInventory() {
 		//displayInventory() shows every character and weapon the player has and shows some details
 		
-		//Iterator variables 
-		Iterator<Character> charIterate = characterInventory.iterator();
-		Iterator<Weapon> weapIterate = weaponInventory.iterator();
-		
 		System.out.println("Inventory:");
 		System.out.printf("\n");
 		
 		//print out every character the player currently has
 		System.out.println("Character: ");
 		
-		while(charIterate.hasNext()) { //loop lasts until the last character in the ArrayList has been reached
-			
-			Character tempChar = charIterate.next(); //tempChar stores the next character in the list
+		for (int i = 0; i < this.characterInventory.size(); i++) { //loop lasts until the size of the arraylist is reached
 			//prints the character's index, name, element, rarity, and level
-			System.out.printf("[%d] Name: %s Element: %s Rarity: %d Level: %d\n", characterInventory.indexOf(tempChar), tempChar.getCharacterName(),
-					  					     tempChar.getCharacterElement(), tempChar.getCharacterRarity(), tempChar.getCharacterLevel());
-			
+			System.out.printf("[%d] Name: %s Element: %s Rarity: %d Level: %d\n", i, this.characterInventory.get(i).getCharacterName(),
+			this.characterInventory.get(i).getCharacterElement(), this.characterInventory.get(i).getCharacterRarity(), this.characterInventory.get(i).getCharacterLevel());
 		}
 		
 		System.out.printf("\n");
 		
-		//print out every weapon the player currently has
 		System.out.println("Weapon: ");
-		while (weapIterate.hasNext()) {
+		
+		for (int i = 0; i < this.weaponInventory.size(); i++) {
 			
-			Weapon tempWeap = weapIterate.next();
-			
-			if (tempWeap.isEquipped()) { //if the weapon is currently equipped on a character:
+			if (this.weaponInventory.get(i).isEquipped()) { //if the weapon is currently equipped on a character:
 				//print the weapon's index, name, power, rarity, level, and the name of the character it is currently equipped to
-				System.out.printf("[%d] Name: %s Power: %d Rarity: %d Level: %d Equipped on: %s", weaponInventory.indexOf(tempWeap), tempWeap.getWeaponName(), 
-						tempWeap.getWeaponPower(), tempWeap.getWeaponRarity(), tempWeap.getWeaponLevel(), tempWeap.getWeaponOwner().getCharacterName());
+				System.out.printf("[%d] Name: %s Power: %d Rarity: %d Level %d Equipped on: %s", i, this.weaponInventory.get(i).getWeaponName(),
+						this.weaponInventory.get(i).getWeaponPower(), this.weaponInventory.get(i).getWeaponRarity(), this.weaponInventory.get(i).getWeaponLevel(),
+						this.weaponInventory.get(i).getWeaponOwner().getCharacterName());
 			}
 			else {
 				//else, print the above details w/o the weapon's owner
-				System.out.printf("[%d] Name: %s Power: %d Rarity: %d Level: %d\n", weaponInventory.indexOf(tempWeap), tempWeap.getWeaponName(), 
-															tempWeap.getWeaponPower(), tempWeap.getWeaponRarity(), tempWeap.getWeaponLevel());
+				System.out.printf("[%d] Name: %s Power: %d Rarity: %d Level %d\n", i, this.weaponInventory.get(i).getWeaponName(), this.weaponInventory.get(i).getWeaponPower(), 
+									this.weaponInventory.get(i).getWeaponRarity(), this.weaponInventory.get(i).getWeaponLevel());
 			}
 		}
    }
